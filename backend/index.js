4a441c6252e6663da94667eff361ee43d730a432
Fanuel Dana <fanueld@meta.com>
2024-06-21 00:42:53 -0700

    final commit

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,6 @@ require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 const PORT = process.env.PORT;
 const boards = require("./routes/allBoards.js");
 const userRoutes = require("./routes/auth.js");
